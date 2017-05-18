36525faaecd33dc0b9018e5a5210965020bbc172
alexstewart96 <s1507747@student.southstaffs.ac.uk>
2017-05-18 11:14:26 +0100

    javascript updated
    create and uploade

--- scripts/MainMenu.js
+++ scripts/MainMenu.js
@@ -1,9 +1,23 @@
 BasicGame.MainMenu = function (game) { };
 
+var startButton;
+var starfield;
+var logo;
+
 BasicGame.MainMenu.prototype = {
 
 	create: function () {
+//we've already loaded the assets so we'll move straight into the MainMenu
+//here all we are doing is playing music adding a picture and a button
+//I willmodify the MainMenu to suit your game
 
+//output sky ship score lives total and start time to the screen
+//the scrolling starfield background
+starfi9eld = this.add.tileSprite(0, 0, 800, 600, 'starfield');
+logo = this.add.sprite((this.world.width / 2), (this.world.height / 2) - 150, 'logo');
+logo.anchor.setTo(0.5,0.5);
+startButton = this.add.buttonj((this.world.width / 2), (this.world.height / 2) + 50, 'startButton', this.startgame);
+startButton.anchor.setTo(0.5,0.5);
 	},
 
 	update: function () {
