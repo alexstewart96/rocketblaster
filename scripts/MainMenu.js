fc7fbd147e202356cd3201b4a8e511d9320f4ee1
alexstewart96 <s1507747@student.southstaffs.ac.uk>
2017-06-15 03:00:54 +0100

    another update

--- scripts/MainMenu.js
+++ scripts/MainMenu.js
@@ -6,7 +6,7 @@ var logo;
 
 BasicGame.MainMenu.prototype = {
 
-	 function create () {
+	create: function () {
 //we've already loaded the assets so we'll move straight into the MainMenu
 //here all we are doing is playing music adding a picture and a button
 //I willmodify the MainMenu to suit your game
@@ -20,13 +20,13 @@ startButton = this.add.button((this.world.width / 2), (this.world.height / 2) + 
 startButton.anchor.setTo(0.5,0.5);
 	},
 
-	 function update () {
+	update: function () {
 		//	Do some nice funky main menu effect here
 	},
 
-	 function startGame () {
+	startGame: function () {
 		//and start the actual game
 		this.game.state.start('game');
-}
+},
 
 };
