8c51108915823a7477c93a76eddbf5fcd5bc7f79
alexstewart96 <s1507747@student.southstaffs.ac.uk>
2017-06-15 01:44:55 +0100

    updated preloader

--- scripts/Preloader.js
+++ scripts/Preloader.js
@@ -25,6 +25,12 @@ BasicGame.Preloader.prototype = {
 	this.load.image('ufo', 'assets/ufo.png');
   this.load.image('life', 'assets/lives.png');
   this.load.image('bullet', 'assets/bullet.png');
+  this.load.spritesheet('kaboom', 'assets/explode.png', 128, 128, 16)
+		;
+this.load.spritesheet('lifeAnimation', 'assets/lifeAnimation.png',100, 100, 4);
+ this.load.audio('music', ['assets/music.m4a', 'assets/music.mp3']);
+ this.load.audio('bullet', ['assets/laser_human.mp3']);
+ this.load.audio('explosion', ['assets/explosion.mp3']);		
 	},
 
 	create: function () {
@@ -32,7 +38,15 @@ BasicGame.Preloader.prototype = {
 	},
 
 	update: function () {
-
+// You don't actually need to do this, but I find it gives a much smoother game experience.
+ // Basically it will wait for our audio file to be decoded before proceeding to the MainMenu.
+ // You can jump right into the menu if you want and still play the music, but you'll have a few
+ // seconds of delay while the mp3 decodes - so if you need your music to be in-sync with your menu
+ // it's best to wait for it to decode here first, then carry on.
+ // If you don't have any music in your game then put the game.state.start line into the create function and delete
+ // the update function completely.
+ if (this.cache.isSoundDecoded('music') && this.ready == false) {
+ this.ready = true;
    this.game.state.start('MainMenu');
 	}
 
