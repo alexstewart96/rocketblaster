f7bed53a4b800d206846263ba8805fc5a62292fd
alexstewart96 <s1507747@student.southstaffs.ac.uk>
2017-06-22 12:15:25 +0100

    Update Preloader.js

--- scripts/Preloader.js
+++ scripts/Preloader.js
@@ -27,7 +27,7 @@ BasicGame.Preloader.prototype = {
   this.load.image('bullet', 'assets/bullet.png');
   this.load.spritesheet('kaboom', 'assets/explode.png', 128, 128, 16);
 this.load.spritesheet('lifeAnimation', 'assets/lifeAnimation.png',100, 100, 4);
- this.load.audio('music', ['assets/oldmusic.m4a', 'assets/space sound.mp3']);
+ this.load.audio('music', ['assets/space sound.mp3']);
  this.load.audio('bullet', ['assets/SoundEffectLaser.mp3']);
  this.load.audio('explosion', ['assets/explosion.mp3']);		
 	},
